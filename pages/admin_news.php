f35d095b6d2589e7c35d01e031820ca18c28b544
nami.d0c.0 <nami.d0c.0@04e9bd74-e1e1-11de-8065-53a27c5290e0>
2009-12-15 17:18:08 +0000

    Modification majeure ;)

--- pages/admin_news.php
+++ pages/admin_news.php
@@ -4,13 +4,44 @@
 		echo ( $perso_msg == NULL )?'Cette news n\'existe pas':$perso_msg;
 		$view->skip_rest_of_page();
 	}
-	validates_get_fields( 'mode' );
+	validates_get_fields( 'mode', 'id' );
 	$mode = $_GET['mode'];
+	$article = NULL;
 	switch( $mode )
 	{
-		case 'new':
+		case 'delete':
+			$article = Doctrine::getTable( T_COORD )->findOne( $_GET['id'] );
+			$article->delete();
+			break;
 		case 'edit':
+			$article = Doctrine::getTable( T_COORD )->findOne( $_GET['id'] );
+			if( isset( $_POST['title'] && isset( $_POST['body'] ) && !empty( $_POST['title'] ) && !empty( $_POST['body'] ) )
+			{
+				$article->title = $_POST['title'];
+				$article->body = $_POST['body'];
+				$article->save();
+			}
+		case 'new':
+			if( isset( $_POST['title'] && isset( $_POST['body'] ) && !empty( $_POST['title'] ) && !empty( $_POST['body'] ) )
+			{
+				$article = new Article();
+				$article->title = $_POST['title'];
+				$article->body = $_POST['body'];
+				$article->save();
+				does_not_exists( 'Article enregistré' );
+			}
 			$form = new Form( array() );
+			$form->input( array(
+					'name' => 'title',
+					'value' => ( $article == NULL )?'':$article->title,
+				), NULL );
+			$form->input( array(
+					'name' => 'body',
+					'value' => ( $article == NULL )?'':$article->body,
+				), NULL );
+			$form->input( NULL, 'submit' );
+			echo $form;
+			break;
 		default:
 			does_not_exists( 'cette action n\'existe pas' );
 	}
